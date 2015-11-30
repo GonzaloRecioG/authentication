7ef2a8e6937eb10b43734500fcc7af7e59d21be0
Gonzalo Recio <gon@reciogarcia.com>
2015-11-30 12:09:06 +0100

    working login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-
+  skip_before_action :authenticate_user!, only: :home #this means you can go to home page with out signing in
+  # before action user sign in ? -> means has to sign in before anything
   def home
   end
+
+
 end
